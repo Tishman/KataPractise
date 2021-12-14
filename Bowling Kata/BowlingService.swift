948009bf7b41c5e4ea695941d69887c8d262ed86
“Roman <r.tishchenko@dodopizza.com>
2021-12-14 17:27:12 +0300

    Implement roll method

--- Bowling Kata/BowlingService.swift
+++ Bowling Kata/BowlingService.swift
@@ -16,7 +16,37 @@ protocol BowlingService {
 final class BowlingServiceImpl: BowlingService {
     
     var game = GameModel()
+    
+    func rollBall(pins: Int) {
+        guard isValid(pins: pins) else { return }
+        
+        guard !game.isLastFrame else {
+            if game.notPlayedFrames.last?.firstRoll == nil {
+                game.notPlayedFrames.last?.firstRoll = pins
+            } else if game.notPlayedFrames.last?.secondRoll == nil {
+                game.notPlayedFrames.last?.secondRoll = pins
+            } else {
+                game.lastFrameBonus = pins
+            }
+            game.playFrame()
+            
+            return
+        }
+        
+        if game.notPlayedFrames.last?.firstRoll == nil {
+            game.notPlayedFrames.last?.firstRoll = pins
+        } else if game.notPlayedFrames.last?.secondRoll == nil {
+            game.notPlayedFrames.last?.secondRoll = pins
+            game.playFrame()
+        }
+    }
+    
     func score() -> Int {
         game.score()
+    }
+    
+    private func isValid(pins: Int) -> Bool {
+        guard pins <= 10, game.notPlayedFrames.count > 0 else { return false }
+        return true
     }
 }
