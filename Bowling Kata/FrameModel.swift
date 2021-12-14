c5ceb185d7251b09eaba696ca7ea80178338a962
“Roman <r.tishchenko@dodopizza.com>
2021-12-14 13:25:16 +0300

    Filled game and frame models

--- Bowling Kata/FrameModel.swift
+++ Bowling Kata/FrameModel.swift
@@ -11,31 +11,45 @@ struct GameModel {
     var head: FrameModel?
     var tail: FrameModel?
     
+    init() {
+        self.head = FrameModel(previous: nil, next: nil)
+        self.tail = nil
+    }
+    
     func score() -> Int {
-        
+        guard head != nil else { return 0 }
+        var temp = head
+        var score = 0
+        while temp != nil {
+            score += temp!.total
+            temp = temp?.next
+        }
+        return score
     }
 }
 
 final class FrameModel {
     var firstRoll: Int?
     var secondRoll: Int?
-    var isClosed: Bool
+    var isStrike: Bool
+    var isSpare: Bool
     let previous: FrameModel?
-    let next: FrameModel
+    let next: FrameModel?
     
     var total: Int {
         return (firstRoll ?? 0) + (secondRoll ?? 0)
     }
     
-    init(firstRoll: Int?, secondRoll: Int?, isClosed: Bool, previous: FrameModel?, next: FrameModel) {
+    init(firstRoll: Int? = nil, secondRoll: Int? = nil, isStrike: Bool, isSpare: Bool, previous: FrameModel?, next: FrameModel?) {
         self.firstRoll = firstRoll
         self.secondRoll = secondRoll
-        self.isClosed = isClosed
+        self.isStrike = isStrike
+        self.isSpare = isSpare
         self.previous = previous
         self.next = next
     }
     
-    convenience init(previous: FrameModel?, next: FrameModel) {
-        self.init(firstRoll: nil, secondRoll: nil, isClosed: false, previous: previous, next: next)
+    convenience init(previous: FrameModel?, next: FrameModel?) {
+        self.init(firstRoll: nil, secondRoll: nil, isStrike: false, isSpare: false, previous: previous, next: next)
     }
 }
