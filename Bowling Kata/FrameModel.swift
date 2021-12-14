0c408a4ed272537f3a3e8931992e999899e141df
“Roman <r.tishchenko@dodopizza.com>
2021-12-14 12:16:41 +0300

    Describe game model

--- Bowling Kata/FrameModel.swift
+++ Bowling Kata/FrameModel.swift
@@ -7,9 +7,35 @@
 
 import Foundation
 
-struct FrameModel {
+struct GameModel {
+    var head: FrameModel?
+    var tail: FrameModel?
+    
+    func score() -> Int {
+        
+    }
+}
+
+final class FrameModel {
     var firstRoll: Int?
     var secondRoll: Int?
-    var isStrike = false
-    var isSpare = false
+    var isClosed: Bool
+    let previous: FrameModel?
+    let next: FrameModel
+    
+    var total: Int {
+        return (firstRoll ?? 0) + (secondRoll ?? 0)
+    }
+    
+    init(firstRoll: Int?, secondRoll: Int?, isClosed: Bool, previous: FrameModel?, next: FrameModel) {
+        self.firstRoll = firstRoll
+        self.secondRoll = secondRoll
+        self.isClosed = isClosed
+        self.previous = previous
+        self.next = next
+    }
+    
+    convenience init(previous: FrameModel?, next: FrameModel) {
+        self.init(firstRoll: nil, secondRoll: nil, isClosed: false, previous: previous, next: next)
+    }
 }
