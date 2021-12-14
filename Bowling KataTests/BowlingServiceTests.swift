a8090170b68012b0d242b9b890b157aade19aa2c
“Roman <r.tishchenko@dodopizza.com>
2021-12-14 11:41:14 +0300

    Refactor tests by corner cases

--- Bowling KataTests/BowlingServiceTests.swift
+++ Bowling KataTests/BowlingServiceTests.swift
@@ -14,185 +14,142 @@ final class BowlingServiceTests: QuickSpec {
     override func spec() {
         describe("\(BowlingService.self)") {
             var bowlingService: BowlingService!
+            var firstRoll = 0
+            var secondRoll = 0
+            var bonus = 0
+            var total: Int { firstRoll + secondRoll + bonus }
             
             beforeEach {
                 bowlingService = BowlingServiceImpl()
             }
             
-            context("when 1st frame playing") {
-                context("and first roll is stike") {
+            context("for closed frame") {
+                context("in strike case") {
                     
-                }
-                
-                context("and first roll is not strike") {
-                    context("but second roll is spare") {
-                        
+                    context("when frame is 1st") {
+                        beforeEach {
+                            bowlingService.rollBall(pins: 10)
+                        }
+                        it("it should have score 10") {
+                            expect(bowlingService.score()).to(be(10))
+                        }
                     }
                     
-                    context("but second roll is not spare") {
+                    context("when frame is 10th") {
+                        context("and three in row") {
+                            beforeEach {
+                                bowlingService.rollBall(pins: 10)
+                            }
+                            
+                            context("for a first roll") {
+                                it("it should have score 10") {
+                                    expect(bowlingService.score()).to(be(10))
+                                }
+                            }
+                            
+                            context("for a second roll") {
+                                it("it should have score 20") {
+                                    expect(bowlingService.score()).to(be(20))
+                                }
+                            }
+                            
+                            context("for a third roll") {
+                                it("it should have score 30") {
+                                    expect(bowlingService.score()).to(be(30))
+                                }
+                            }
+                        }
                         
+                        context("and two in row") {
+                            beforeEach {
+                                bowlingService.rollBall(pins: 10)
+                            }
+                            
+                            context("for a first roll") {
+                                it("it should have score 10") {
+                                    expect(bowlingService.score()).to(be(10))
+                                }
+                            }
+                            
+                            context("for a second roll") {
+                                it("it should have score 20") {
+                                    expect(bowlingService.score()).to(be(20))
+                                }
+                            }
+                            
+                            context("for a third roll") {
+                                beforeEach {
+                                    bowlingService.rollBall(pins: 5)
+                                }
+                                
+                                it("it should have score 25") {
+                                    expect(bowlingService.score()).to(be(25))
+                                }
+                            }
+                        }
+                    }
+                    
+                    context("when several strikes") {
+                        beforeEach {
+                            
+                        }
+                    }
+                    
+                    context("when strike is single in middle game") {
+                        
+                    }
+                }
+                
+                context("in spare case") {
+                    beforeEach {
+                        firstRoll = 5
+                        secondRoll = 5
+                        bowlingService.rollBall(pins: firstRoll)
+                        bowlingService.rollBall(pins: secondRoll)
+                    }
+                    
+                    context("when frame is 1st") {
+                        it("it should have score \(firstRoll + secondRoll)") {
+                            expect(bowlingService.score()).to(be(firstRoll + secondRoll))
+                        }
+                    }
+                    
+                    context("when frame is 10th") {
+                        beforeEach {
+                            bonus = 5
+                            bowlingService.rollBall(pins: bonus)
+                        }
+                        it("it should have score \(total)") {
+                            expect(bowlingService.score()).to(be(total))
+                        }
+                    }
+                    
+                    context("when spare is single in middle game") {
+                        beforeEach {
+                            bonus = 5
+                            bowlingService.rollBall(pins: bonus)
+                        }
+                        
+                        it("it should have score \(total)") {
+                            expect(bowlingService.score()).to(be(total))
+                        }
+                        it("next frame first roll score should be equal bonus") {
+                            
+                        }
                     }
                 }
             }
             
-            context("when 2nd frame playing") {
-                context("and first roll is stike") {
-                    context("but previous frame is striked") {
-                        
+            context("for opened frame") {
+                context("in simple case") {
+                    beforeEach {
+                        firstRoll = 4
+                        secondRoll = 4
+                        bowlingService.rollBall(pins: firstRoll)
+                        bowlingService.rollBall(pins: secondRoll)
                     }
-                }
-                
-                context("and first roll is not strike") {
-                    context("but second roll is spare") {
-                        
-                    }
-                    
-                    context("but second roll is not spare") {
-                        
-                    }
-                }
-            }
-            
-            context("when 3rd frame playing") {
-                context("and first roll is stike") {
-                    context("but previous frame is striked") {
-                        
-                    }
-                }
-                
-                context("and first roll is not strike") {
-                    context("but second roll is spare") {
-                        
-                    }
-                    
-                    context("but second roll is not spare") {
-                        
-                    }
-                }
-            }
-            
-            context("when 4th frame playing") {
-                context("and first roll is stike") {
-                    context("but previous frame is striked") {
-                        
-                    }
-                }
-                
-                context("and first roll is not strike") {
-                    context("but second roll is spare") {
-                        
-                    }
-                    
-                    context("but second roll is not spare") {
-                        
-                    }
-                }
-            }
-            
-            context("when 5th frame playing") {
-                context("and first roll is stike") {
-                    context("but previous frame is striked") {
-                        
-                    }
-                }
-                
-                context("and first roll is not strike") {
-                    context("but second roll is spare") {
-                        
-                    }
-                    
-                    context("but second roll is not spare") {
-                        
-                    }
-                }
-            }
-            
-            context("when 6th frame playing") {
-                context("and first roll is stike") {
-                    context("but previous frame is striked") {
-                        
-                    }
-                }
-                
-                context("and first roll is not strike") {
-                    context("but second roll is spare") {
-                        
-                    }
-                    
-                    context("but second roll is not spare") {
-                        
-                    }
-                }
-            }
-            
-            context("when 7th frame playing") {
-                context("and first roll is stike") {
-                    context("but previous frame is striked") {
-                        
-                    }
-                }
-                
-                context("and first roll is not strike") {
-                    context("but second roll is spare") {
-                        
-                    }
-                    
-                    context("but second roll is not spare") {
-                        
-                    }
-                }
-            }
-            
-            context("when 8th frame playing") {
-                context("and first roll is stike") {
-                    context("but previous frame is striked") {
-                        
-                    }
-                }
-                
-                context("and first roll is not strike") {
-                    context("but second roll is spare") {
-                        
-                    }
-                    
-                    context("but second roll is not spare") {
-                        
-                    }
-                }
-            }
-            
-            context("when 9th frame playing") {
-                context("and first roll is stike") {
-                    context("but previous frame is striked") {
-                        
-                    }
-                }
-                
-                context("and first roll is not strike") {
-                    context("but second roll is spare") {
-                        
-                    }
-                    
-                    context("but second roll is not spare") {
-                        
-                    }
-                }
-            }
-            
-            context("when 10th frame playing") {
-                context("and first roll is stike") {
-                    context("but previous frame is striked") {
-                        
-                    }
-                }
-                
-                context("and first roll is not strike") {
-                    context("but second roll is spare") {
-                        
-                    }
-                    
-                    context("but second roll is not spare") {
-                        
+                    it("it should have score \(firstRoll + secondRoll)") {
+                        expect(bowlingService.score()).to(be(firstRoll + secondRoll))
                     }
                 }
             }
