ab5676d7858098849425565346284831165a802d
“Roman <r.tishchenko@dodopizza.com>
2021-12-14 17:26:52 +0300

    Change frame and game models

--- Bowling Kata/FrameModel.swift
+++ Bowling Kata/FrameModel.swift
@@ -10,14 +10,10 @@ import Foundation
 struct GameModel {
     private var _notPlayedFrames: [FrameModel]
     private var _playedFrames: [FrameModel]
-    private var _lastFrameBonus = 0
+    var lastFrameBonus = 0
     
     var notPlayedFrames: [FrameModel] { _notPlayedFrames }
     var playedFrames: [FrameModel] { _playedFrames }
-    
-    var score: Int {
-        _notPlayedFrames.map(\.total).reduce(0, +)
-    }
     
     var isFirstFrame: Bool {
         _notPlayedFrames.count == 10
@@ -36,41 +32,29 @@ struct GameModel {
         _playedFrames.append(_notPlayedFrames.removeLast())
     }
     
-    mutating func strikeRolled() {
-        if isFirstFrame {
-            _notPlayedFrames.last?.firstRoll = 10
-            _notPlayedFrames.last?.isStrike = true
-        } else if isLastFrame {
-            configureLastFrame()
-        } else {
-            _notPlayedFrames.last?.firstRoll = 10
-            _notPlayedFrames.last?.isStrike = true
-            for step in 0...1 {
-                let index = (_playedFrames.count - 1) - step
-                guard _playedFrames[index].isStrike else { break }
-                _playedFrames[index].firstRoll! += 10
-            }
-        }
+    func score() -> Int {
+      
         
-        playFrame()
-    }
-    
-    private mutating func configureLastFrame() {
-        if _notPlayedFrames.last?.firstRoll == nil {
-            _notPlayedFrames.last?.firstRoll = 10
-        } else if _notPlayedFrames.last?.secondRoll == nil {
-            _notPlayedFrames.last?.secondRoll = 10
-        } else {
-            _lastFrameBonus = 10
-        }
     }
 }
 
 final class FrameModel {
-    var firstRoll: Int?
-    var secondRoll: Int?
-    var isStrike: Bool
-    var isSpare: Bool
+    var firstRoll: Int? {
+        didSet {
+            if firstRoll == 10 {
+                isStrike = true
+            }
+        }
+    }
+    
+    var secondRoll: Int? {
+        didSet {
+            if firstRoll != 10 && total == 10 { isSpare = true }
+        }
+    }
+    
+    private var isStrike: Bool
+    private var isSpare: Bool
     
     var total: Int {
         return (firstRoll ?? 0) + (secondRoll ?? 0)
